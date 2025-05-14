ee4df0330fb404064a8c0088bff2f62405665237
ian <ianwisdom90@gmail.com>
2025-05-14 10:32:32 +0300

    changes od init

--- strays_heven.py
+++ strays_heven.py
@@ -1,7 +1,31 @@
+#class initialization with default_init_
+
 class Pet:
-    pass
+    def speak(self):
+        print("sound made")
+        
+
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())
+
+#a simple class to represent dog with modified _init_
 class Dog:
-    pass
+    def __init__(self,name,breed,age = "N/A"):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def speak(self):
+        return f"{self.name} says Woof!"
+koba = Dog("Koba","Pitbull",2)
+amad = Dog("Amad","Pitbull",)
+print(koba.speak())
+print(koba.age)
+print(amad.age)
+    
 class Cat:
     pass
 class rat:
+    pass
